0aec113b07a38763b378804589971867e1db0319
EmmaOkafor <okaforifebuche26@gmail.com>
2024-08-30 13:35:46 +0100

    Added navigator to get location

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
 	const [isFetching, setIsFetching] = useState(false);
@@ -17,15 +18,23 @@ export default function AvailablePlaces({ onSelectPlace }) {
 				if (!response.ok) {
 					throw new Error('Failed to fetch places');
 				}
-				setAvailablePlaces(resData.places);
+
+				navigator.geolocation.getCurrentPosition((position) => {
+					const sortedPlaces = sortPlacesByDistance(
+						resData.places,
+						position.coords.latitude,
+						position.coords.longitude
+					);
+					setAvailablePlaces(sortedPlaces);
+					setIsFetching(false);
+				});
 			} catch (error) {
 				setError({
 					message:
-						error.message || 'Could not fetch places, please try again later',
+						error.message || 'Could not fetch places, please try again later.',
 				});
+				setIsFetching(false);
 			}
-
-			setIsFetching(false);
 		}
 
 		fetchPlaces();
