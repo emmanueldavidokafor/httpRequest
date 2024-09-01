7f7cf29c0e7c249fa685500dbe25b158d46c88ca
EmmaOkafor <okaforifebuche26@gmail.com>
2024-09-01 14:51:01 +0100

    Finished project

--- src/http.js
+++ src/http.js
@@ -9,6 +9,17 @@ export async function fetchAvailablePlaces() {
 	return resData.places;
 }
 
+export async function fetchUserPlaces() {
+	const response = await fetch('http://localhost:3000/places');
+
+	if (!response.ok) {
+		throw new Error('Failed to fetch places');
+	}
+	const resData = await response.json();
+
+	return resData.places;
+}
+
 export async function updateUserPlaces(places) {
 	const response = await fetch('http://localhost:3000/user-places', {
 		method: 'PUT',
